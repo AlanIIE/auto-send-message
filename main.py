5d81861426acb00a081a00b332a479acb3b19e7c
AlanIIE <dongliang.ucas@gmail.com>
2022-09-04 10:28:05 +0800

    support image

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import time
 from random import choice
 from PyQt5 import QtCore,QtWidgets
 from datetime import datetime
+from PIL import Image
+from io import BytesIO
+import numpy as np
 
 class sendMsg():
     def __init__(self,receiver,msg):
@@ -13,7 +16,7 @@ class sendMsg():
         self.msg=msg
 
     def winUpLoadFile(self):
-        # 将文件复制到剪切板
+        # copy file to clipboard
         app = QtWidgets.QApplication([])
         data = QtCore.QMimeData()
         url = QtCore.QUrl.fromLocalFile(self.msg)
@@ -24,36 +27,50 @@ class sendMsg():
         self.sendmsg()
     
     def sendText(self):
-        #设置剪贴版内容
+        # set text in clipboard
         w.OpenClipboard()
         w.EmptyClipboard()
         w.SetClipboardData(win32con.CF_UNICODETEXT, self.msg)
         w.CloseClipboard()
+        self.sendmsg()
 
-		self.sendmsg()
-    #发送消息
+
+    def sendImage(self):
+        # load image as Byte data
+        output = BytesIO()
+        self.msg.save(output, 'BMP')
+        data = output.getvalue()[14:]
+        output.close()
+        # set text in clipboard
+        w.OpenClipboard()
+        w.EmptyClipboard()
+        w.SetClipboardData(w.CF_DIB, data)
+        w.CloseClipboard()
+
+        self.sendmsg()
+    # send message
     def sendmsg(self):
         qq=win32gui.FindWindow(None,self.receiver)
-        win32gui.GetClassName(qq)  # 获取窗口classname
-        title = win32gui.GetWindowText(qq)  # 获取窗口标题
+        win32gui.GetClassName(qq)  # get window classname
+        title = win32gui.GetWindowText(qq)  # get window title
         win32gui.GetDlgCtrlID(qq)
-        win32gui.SetForegroundWindow(qq)  # 激活窗口
+        win32gui.SetForegroundWindow(qq)  # activate window
 
-		#粘贴内容
-        win32api.keybd_event(17, 0, 0, 0)  # ctrl键位码是17
-        win32api.keybd_event(86, 0, 0, 0)  # v键位码是86
-        win32api.keybd_event(86, 0, win32con.KEYEVENTF_KEYUP, 0)  # 释放按键
+        # ctrl+v
+        win32api.keybd_event(17, 0, 0, 0)  # ctrl = 17
+        win32api.keybd_event(86, 0, 0, 0)  # v = 86
+        win32api.keybd_event(86, 0, win32con.KEYEVENTF_KEYUP, 0)  # release key
         win32api.keybd_event(17, 0, win32con.KEYEVENTF_KEYUP, 0)
 
-		#发送内容
+        # send message
         win32api.keybd_event(18, 0, 0, 0)  # Alt
         win32api.keybd_event(83, 0, 0, 0)  # s
-        win32api.keybd_event(83, 0, win32con.KEYEVENTF_KEYUP, 0)  # 释放按键
+        win32api.keybd_event(83, 0, win32con.KEYEVENTF_KEYUP, 0)  # release key
         win32api.keybd_event(18, 0, win32con.KEYEVENTF_KEYUP, 0)
 
         print("sucessfuly send",self.msg)
 
-# 从文件中读取文字
+# load text frome file
 def getmessage(fileName):
     f=open(fileName,'r',encoding='utf-8')
     lines=f.readlines()
@@ -61,18 +78,27 @@ def getmessage(fileName):
     return choice(lines)
 
 def main():
-    receiver='测试群聊'#这里填入接收者的备注名 
-    date_sche = 17 # 17日
-    time_sche = 15 # 15时。需要更精确的时间可以调整后面的if条件以及sleep时间
+    receiver='测试群聊'# fill the receiver's nickname
+    date_sche = 17 # 17th
+    time_sche = 15 # 15:00. More acurrate time can be set in 'time.sleep()' later
     while True:
         date_now = datetime.now()
         if date_now.day == date_sche and date_now.hour == time_sche:
-        	filename = 'G:\\Users\\1\\Desktop\\英语作业.docx' # 设置文件
-            qq=sendMsg(receiver, filename) # 初始化
-            qq.winUpLoadFile() # 发送文件
-            # msg = getmessage('message.txt') # 设置消息
-            # qq=sendMsg(receiver, msg) # 初始化
-            # qq.sendText() # 发送消息
+            filename = r'D:\Users\liang\Desktop\英语作业.docx' # set path for file
+            qq=sendMsg(receiver, filename) # init
+            qq.winUpLoadFile() # sent file
+            
+            # msg = getmessage('message.txt') # set text
+            # qq=sendMsg(receiver, msg) # init
+            # qq.sendText() # send text
+	
+            # load image file with PIL.
+            # better for sending calculated images, no need to save it in HDD
+            # if send file in disk, try qq.winUpLoadFile()
+            img = np.random.randint(0, 255, [1800,3200,3], dtype=np.uint8)
+            pil_img = Image.fromarray(img)
+            qq=sendMsg(receiver, msg=pil_img)
+            qq.sendImage() # send image
         time.sleep(3600)
 
 if __name__ == '__main__':
